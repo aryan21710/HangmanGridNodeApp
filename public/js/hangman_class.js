3621cd9ccce9456fc613f37deef5bfe3a0cbcfb0
aryan21710 <aryan21710>
2019-02-22 12:15:31 +0530

    ADDED SEQUENTIAL COMMENTS IN ALL THE FILES. IMPLEMENTED WEAKMAP AND IIFE

--- public/js/hangman_class.js
+++ public/js/hangman_class.js
@@ -7,34 +7,12 @@ class HangmanClass {
         this.score=score;
         this.movieName=movieName;
     }
-
-    static createTags(movie) {
-        console.log('*********createTags is called***********');
-        // console.log('INSIDE STATIC:-'+movie);
-        document.getElementById('userInpDiv').innerHTML='<input type="text" maxlength="1" id="userInpTag" \
-        style="color: white;  \
-         text-align: center;" placeholder="ENTER TEXT HERE">'
-
-        const inputTags=[];
-        const wrongOptions=[];
-        const wrongOptionsId=[];
-        movie.split('').forEach((v,ind)=>{
-            v != ' ' ? inputTags.push(`<input disabled id=${v.toUpperCase()} style="color: white; background-image: url('brick-black-wallpaper.jpg'); width: 30px; height: 30px; font-size: 1.5em; text-align: center;" maxlength="1">`) : inputTags.push('&nbsp&nbsp');
-            v != ' ' ? wrongOptions.push(`<span id=${ind} style="margin-left: 20px; text-align: center; display: inline;"></span>`): '';
-            v != ' ' ? wrongOptionsId.push(ind): '';
-        })
-        // console.log('inputTags:-'+inputTags);
-        const userInput=[];
-        // console.log(':'+wrongOptions);
-        document.getElementById('WrongOptions').innerHTML=wrongOptions.join('');
-        document.getElementById('mydiv').innerHTML=inputTags.join('');
-        return wrongOptionsId;
-    }
-
   
     static removeFoundLetter(dupmovieVar,letter) {
         console.log('***********removeFoundLetter is called***********');
 
+// 32] USING dupmovieVar Array(movieName) look for alphabet entered by user in it. MAKE A NEW ARRAY
+// called newmovie AFTER REMOVING THE MATCHED LETTER(alphabet entered by user) FROM THE MOVIENAME FOR FURTHER SEARCH.
         dupmovieVar=dupmovieVar.split('');
         let newmovie=[];
         for (let i=0; i < dupmovieVar.length; i++) {
@@ -42,6 +20,7 @@ class HangmanClass {
         }
         newmovie=newmovie.join('');
         console.log(`after removing ${letter} the new movie name is ${newmovie}`);
+// 33] RETURN THE NEW ARRAY 'newmovie' WITH ANYMORE UNMATCHED LETTERS IN IT. 
         return newmovie;        
     }
 
@@ -70,13 +49,15 @@ class HangmanClass {
             document.getElementById('audio').src='Kid Laughing.mp3';    
         } else if (type=='won') {
             document.getElementById('audio').src='Applause Light.mp3';    
-        }
+        } 
     }
 
     static printWinMessage(status) { 
         console.log('status inside printWinMessage:-'+status);
 
         // console.log('intial fontsize:-'+document.getElementById('PrintStatusCont').style.fontSize);
+// 44] IF status ARGUMENT PASSED TO printWinMessage IS win or lose THEN CHANGE THE FONT SIZE OF
+// '#PrintStatusCont div' TO 0.3em TO ENABLE THE YOU WON OR YOU LOST ANIMATION.
         if (status=='win' || status=='lose') {
             document.querySelector('#PrintStatusCont div').style.fontSize='0.3em';
             this.printMessage=()=> {
@@ -95,15 +76,19 @@ class HangmanClass {
                     document.querySelector('#PrintStatusCont div').innerHTML="YOU WON..";
                     console.log('after:-'+document.querySelector('#PrintStatusCont div').innerHTML);
                 } else {
+// 46] SEND THE WIN LOSE GAME STATUS TO SERVER USING EMTI EVENT.
                     console.log('in else block of printWinMessage');
                     socket.emit('gameStatus',{
                         scr: status,
-                    })          
+                    })
+// 48] CLEAR THE INTERVAL STOP THE ANIMATION OF FONT SIZE ONCE FONT-SIZE IS > 1.5          
                     clearInterval(clearInt);
                 }
              
 
             }
+    // 45] CALL THE function 'this.printMessage' TO CHANGE THE FONTSIZE EVERY 300 MSEC AND PRINT THE
+    // MESSAGE YOU WON OR YOU LOSE.
             let clearInt=setInterval(this.printMessage,300);
 
         }
@@ -113,11 +98,16 @@ class HangmanClass {
      checkResult(result,movie,animationCount) {
         console.log('***********checkresult is called***********');
         console.log('animationCount:-'+animationCount);
+//41] LETTERS ENTERED BY USER IS STORED IN RESULT IS CONVERTED INTO UPPERCASE LETTERS
         result=result.toUpperCase();
         movie=movie.split(' ').join('').toUpperCase();
         console.log(result+':'+movie);
       
         console.log('CURRENT SCORE:-'+this.score);
+// 42] TO DECIDE WHETHER USER WON a] animationCount IS GREATER THAN 0.AND LENGTH OF MOVIENAME IS SAME AS RESULT (WHICH 
+// CONTAINS ALL THE LETTERS ENTERED BY USER). ONE BY ONE EVERY ELEMENT INSIDE RESULT STRING IS MATCHED WITH
+// MOVIENAME IF ONE OF IT DOESNT MATCH, USER LOSES. IF ALL OF THEM MATCHES USER WINS.
+
         if (animationCount > 0) {
             if (result.length===movie.length) {
                 for(let i=0;i < result.length; i++) {
@@ -125,29 +115,40 @@ class HangmanClass {
                         console.log(`${result[i]} didnt match. User will Play next Move`);                
                     }
                 }
+// 43] IF ALL MATCHES CALL function 'printWinMessage'. CALL THE RIGHT SOUND EFFECTS ACCORDINLY
                 HangmanClass.printWinMessage('win');
+                document.getElementById('userInpTag').value='HIT START TO PLAY AGAIN'
+
                 HangmanClass.soundEffects('won');
+// 49] DISABLE THE BOX 'ENTER TEXT HERE' FOR ANY MORE INPUT BY USER SINCE GAME IS OVER.
                 document.getElementById('userInpTag').disabled=true;
+// 50] INCREASE THE SCORE BY +10 AND EMIT THE SAME TO SERVER.
                 this.score+=10;
                 socket.emit('clToSvScr',this.score);
 
-                
-                setTimeout(()=>{
+//52] DISPLAY THE SCORE AFTER 1 SEC.                
+             setTimeout(()=>{
                     document.querySelector('span').innerHTML=this.score;
                 }, 1000);
                 console.log('CHANGED SCORE:-'+this.score);
             }
         } else {
+// 53] IF ALL CONDITION IN 42 STEP FAILS, KILL THE USER.
             HangmanClass.soundEffects('kill');
-            console.log('NO OF CHANCES ARE EXHAUSTED..CALLING printMessage AFTER THIS');
+            console.log('NO OF CHANCES ARE EXHAUSTED...CALLING printMessage AFTER THIS');
+// 54] CALL function 'printWinMessage' WITH STATUS LOSE AND WITH THE UNCHANGED SCORE.
             HangmanClass.printWinMessage('lose');
             document.getElementById('mydiv').style.font='2em bold';
             document.getElementById('mydiv').style.fontFamily='ZCOOL KuaiLe, cursive';
             
+//55] CHANGED THE FONT SIZE OF mydiv TO BE LIL BIGGER 2em AND PRINT THE MOVINAME SINCE GAME IS OVER
+// FOR USER REFERENCE.            
             document.getElementById('mydiv').innerHTML=this.movieName;
+            document.getElementById('userInpTag').value='GAME OVER. CLICK START'
            
             document.getElementById('userInpTag').disabled=true;   
             // this.score > 0 ? this.score-=10 : this.score=0;
+// 56] EMIT THE UPDATED SCORE TO SERVER
             socket.emit('clToSvScr',this.score);
 
             setTimeout(()=>{
@@ -227,67 +228,144 @@ class HangmanClass {
                 console.log('myCanvas element inside AnimationArea missing');
         }
     }
+
+// 15] hangman.start() IS CALLED
     
      start() {
 
-        console.log('***************** NEW GAME ************************');
+        console.log('***************** NEW GAME **********************');
+
+// 16] FETCHING ONE RANDOM MOVIE FROM THE MOVIENAME VARIABLE WHICH CONTAINS NAME OF ALL MOVIES.
 
         this.movieName=this.movieArr[Math.floor(Math.random()*this.movieArr.length-1)].trim();
-        // // console.log('moviename:-'+this.movieName.split(''));
-        // this.movieArr.forEach((v)=>{
-        //     console.log(v+':'+v.trim().length);
-        // })
-
         console.log('movieName:-'+this.movieName+':'+this.movieName.length); 
+
+//17] shapeArr IS INSTANTIATED WHICH CONTAINS WHICH HANGMAN ANIMATION TO CALL WHEN USER MISSES A MOVE.
+
         this.shapeArr=['Head','Body','LeftLeg','RightLeg','LeftHand','Kill'];
         console.log('this.shapeArr:-'+this.shapeArr);
         console.log('moviename:-'+this.movieName);
+
+// 17] MAKING THE CONTENTS OF '#PrintStatusCont div' TO NULL FROM HANGMAN CHALLENGE AS SET IN INDEX.JS EARLIER.
         document.querySelector('#PrintStatusCont div').innerHTML='';
         document.querySelector('#PrintStatusCont div').style.fontSize='1.5em';
+
+// 18] INSTANTIATING rightOptionMsg ARRAY WHICH CONTAINS ALL THE MESSAGES TO BE DISPLAYED WHEN USER MOVE
+// IS CORRECT.
         let rightOptionMsg=['KYA BAAT HAI','SUPER MACHA','KEEP GOING','EK NUMBER','TUSSI CHA GAYE','GAMER ON FIRE', 'TUMBA CHENNAGIDE','KHUB BHALO', 'BAHU SARAS'];
         const AnimationArea=document.getElementById('AnimationArea');
+
+//19] INTIALISING THE 'AnimationArea' DIV FOR THE HANGMAN ANIMATION USING CANVAS.
         AnimationArea.innerHTML='<canvas id="myCanvas" width= "280" height = "580"> </canvas>'
+
+// 20] ANIMATION FUNCTION IS CALLED NOW.
         this.animation(null);
         let result='';
+
         let dupmovieVar=this.movieName;
-        let wrongOptionsId=HangmanClass.createTags(this.movieName);
+
+        wm.set(this,()=>{
+            //22] CREATING A TEXTBOX INSIDE 'userInpDiv' CALLED 'userInpTag' WHERE USER CAN ENTER THE TEXT.
+            document.getElementById('userInpDiv').innerHTML='<input type="text" maxlength="1" id="userInpTag" \
+            style="color: white;  \
+            text-align: center;" placeholder="GUESS THE MOVIE">'
+
+            const inputTags=[];
+            const wrongOptions=[];
+            const wrongOptionsId=[];
+            // 23] THE MOVIENAME array WHICH CONTAINS NAME OF A MOVIE {INCLUDING SPACES} IS USED TO MAKE SAME AMOUNT
+            // OF TEXTBOXES IN THE FORM OF inputTags and wrongOptions. ALSO THE NAME OF MOVIE IS SPLIT INTO ALPABETS
+            // AND THOSE ALPHABETS ARE ASSIGNED TO EVERY TEXTBOX INSIDE inputTags array WHICH IS LATER ASSIGNED
+            // TO DIV 'mydiv'
+
+
+            this.movieName.split('').forEach((v,ind)=>{
+                v != ' ' ? inputTags.push(`<input disabled id=${v.toUpperCase()} style="color: white; background-image: url('brick-black-wallpaper.jpg'); width: 30px; height: 30px; font-size: 1.5em; text-align: center;" maxlength="1">`) : inputTags.push('&nbsp&nbsp');
+                v != ' ' ? wrongOptions.push(`<span id=${ind} maxlength="1" style="margin-left: 20px; text-align: center; display: inline;"></span>`): '';
+                v != ' ' ? wrongOptionsId.push(ind): '';
+            })
+            // console.log('inputTags:-'+inputTags );
+            const userInput=[];
+            // console.log(':'+wrongOptions);
+            //24] THE WRONGOPTIONS ARRAY WHICH CONTAINS SAME AMOUNT OF SPAN TAGS AS THE NO OF LETTERS IN THE MOVIENAME IS
+            // CREATED INSIDE DIV 'WrongOptions'
+            document.getElementById('WrongOptions').innerHTML=wrongOptions.join('');
+            document.getElementById('mydiv').style.font='';
+
+            //25] THE inputTags ARRAY WHICH CONTAINS SAME AMOUNT OF textboxes TAGS AS THE NO OF LETTERS IN THE MOVIENAME IS
+            // CREATED INSIDE DIV 'mydiv'.
+
+            document.getElementById('mydiv').innerHTML=inputTags.join('');
+
+            //26] THE FUNCTION RETURNS the 'wrongOptionsId' ARRAY WHICH CONTAINS INDEXS OF moviename;
+            return wrongOptionsId;
+
+        })
+
+        //21] CALLING THE WEAKMAP FUNCTION WITH ONE MOVIENAME AS AN ARGUMENT TO CREATE TEXTBOXES 
+        // WHERE USER CAN ENTER THE ALPHABET's
+        // AND SAME BEING DISPLAYED ON MATCH OR NO MATCH ON THE UI IN THEIR RESPECTIVE PLACES/DIV's
+        let wrongOptionsId=wm.has(this)? wm.get(this): console.log('NOT ABLE TO CREATE TAGS');
+        wrongOptionsId=wrongOptionsId();
+
         let keyDwnFunc=((e)=>{
                 e.target.value=e.target.value.toUpperCase();
                 console.log('entered :-'+e.target.value);
+        // 28] alphabet entered by user is stored and made an ID with the same alphabet name. eg:-#A, #X.
                 let idName='#'+e.target.value;
+
+        // 29] dupMovieVar is duplicate value of this.moviename to preserve this.movieName for comparision purposes.
+
+        //30] if dupMovieVar(movieName) includes the alphabet entered by user, find all the id set in step 23 and 
+        // mark the value of all those tags which has same ID'S as the alphabet entered by user. The value marked
+        // will have a] new ID to some random value(here no 9, #9) to take it out of search process. b] ALL the textboxes
+        // tag will be displayed with alphabet entered by user. result is a string which contains all the LETTERS
+        // ENTERED BY USER.
+
                 if (dupmovieVar.includes(e.target.value)) {
                         document.querySelectorAll(idName).forEach((v)=>{
                             v.value=e.target.value;
                             v.id='9';
                             result+=e.target.value;
                         }) 
+// 31] Call removeFoundLetter FUNCTION with alphabet entered by user and moviename. dupmovieVar becomes 'newmovie'
+// as in STEP 33]
                     dupmovieVar=HangmanClass.removeFoundLetter(dupmovieVar,e.target.value);
+//34] SINCE ITS MATCH AS PER STEP 30, DISPLAY ONE OF THE RANDOM TEXT FROM ARRAY rightOptionMsg inside
+// '#PrintStatusCont div'
                     console.log('rightOptionMsg1.:-'+ rightOptionMsg[Math.floor(Math.random()*rightOptionMsg.length)]);
                     document.querySelector('#PrintStatusCont div').style.fontSize='1.5em';
                     document.querySelector('#PrintStatusCont div').innerHTML= rightOptionMsg[Math.floor(Math.random()*rightOptionMsg.length)];
+
+//35] ENABLE THE SOUNDEFFECTS BY CALLING soundEffects FUNCTION FOR THE RIGHT OPTION.
                     HangmanClass.soundEffects('right');
 
                     } else {
+//36] CALL THE wrngOption function when movieName doesnt include the ALPHABET ENTERED BY USER.
                         HangmanClass.wrngOption(e.target.value,wrongOptionsId);
+//37] SINCE THE LETTER ENTERED BY USER IS WRONG DONT DISPLAY ANY MESSAGE IN THE '#PrintStatusCont div'
                         document.querySelector('#PrintStatusCont div').innerHTML='';
+//38] CALL THE HANGMAN ANIMATION TO KILL ONE BODY PART NOW AND THEN CALL THE WRONG OPTION SOUNDEFFECT.
                         if(this.shapeArr.length>0) {
                             this.shape=this.shapeArr.shift();
                             this.animation(this.shape,this.shapeArr.length);  
                             HangmanClass.soundEffects('wrong');
                         } 
                     }
-                    e.target.value='';               
+// 39] MAKE THE TEXTBOX 'ENTER TEXT HERE' BLANK AGAIN FOR USER TO ENTER NEW LETTER.
+                    e.target.value='';       
+                    
+// 40] CALL THE FUNCTION checkResult WITH THE result,moviename and NO OF OPTIONS LEFT BEFORE THE
+// FINAL KILL EVENT.
                     this.checkResult(result,this.movieName,this.shapeArr.length);
             })
+
+    // 27] Attach a keyDwnFunc on 'userInpTag' which will have the alphabets entered by user.
             document.getElementById('userInpTag').addEventListener('input',keyDwnFunc);   
     }
 
-  
-
-
 }
 
-// const hangman=new HangmanClass('XQ JNHG PRTYW');
-// const hangman=new HangmanClass('TAXI DRIVER');
-// const hangman=new HangmanClass('FANNY AND ALEXANDER');
-// // const hangman=new HangmanClass('INDIANA JONES AND THE LAST CRUSADE') --> UI breaking
+let wm=new WeakMap(); 
+
+
--- public/js/index.js
+++ public/js/index.js
@@ -4,28 +4,33 @@ const socket=io();
 // DONT USE ARROW FUNCTION AS THE ES6 FEATURES MIGHT NOT BE 
 // COMPATIBLE WITH MOBILE BROWSER ON MOBILE PHONE. INSTEAD
 // USE NORMAL ES5 FUNCTION SYNTAX.
+
+// 1b] LET THE CLIENT CONNECT TO THE SERVER USING SOCKET connect METHOD. AND ONCE THE CLIENT IS
+// CONNECTED AND WEBPAGE IS READY ON THE LANDING PAGE DISPLAY HANGMAN CHALLENGE IN THE #PrintStatusCont div
     socket.on('connect',()=> {
     console.log('CONNECTED TO SERVER');
+    document.querySelector('#PrintStatusCont div').innerHTML='THE HANGMAN CHALLENGE'
     socket.on('disconnect',()=> {
         console.log('DISCONNECTED TO SERVER');
     });
 });
 
+// 8] TOPSCORE WILL BE RECEIVED BY CLIENT IN THE FORM OF STRING 'topScorers'.
+
+
 socket.on('topScorers',(topScorers)=>{
     console.log('topScorers on client:-'+topScorers);
+// 9] THE TOPSCORER STRING WILL BE DISPLAYED INSIDE THE DIV 'TopUserText' NOW.
     document.getElementById('TopUserText').innerHTML='TOP SCORERS:-' + topScorers;
 })
 
 
-socket.on('sendMovieList',(obj)=>{
-    console.log('sendMovielist received on client..:-'+obj.scr+':'+obj.movieName+':'+obj.movieName.length);
-    const hangman=new HangmanClass(obj.movieName,undefined,obj.scr,undefined);
-    hangman.start();
-    
-})
 
 
-function enterNameAnimation() {
+// 1] BODY ONLOAD enterNameAnimation FUNCTION IS CALLED WHEN HTML PAGE IS LOADED;
+
+// 2] SET THE ANIMATION TO BE BLINKING ON DIV 'userInpTag' WHERE USERNAME IS ENTERED BY USER.
+let enterNameAnimation=(function() {
     let cnt=0;
     let countdown=setInterval(()=>{
         if (cnt % 2==0 && cnt < 10) {
@@ -38,8 +43,71 @@ function enterNameAnimation() {
         cnt++;
     },100);
 
+}())
+
+// 3] THE USERNAME ENTERED IN HTML PAGE WILL ATTACH KEYPRESS EVENT on 'userInpTag' WHICH WILL STORE THE 
+// VALUE OF 'username'
+function userOptionsInit(event) {   
+    const username=[];
+    console.log('inside:-userOptionsInit:-');
+    event.preventDefault();
+    document.getElementById('userInpTag').addEventListener('keypress',(event)=>{
+        console.log('event.keyCode:-'+event.keyCode);
+        event.keyCode==13? event.preventDefault() : "";
+    })
+    console.log(event.target.value);
+    username.push(event.target.value);
+
+// 4] THE ENTERED USERNAME WILL BE EMITTED BY CLIENT TO SERVER.
+    username.forEach((v)=>{
+        console.log('emitting:-'+v);
+        socket.emit('userName',v);
+    }); 
+
+// 5] THE movieOptionAnimation FUNCTION WILL BE CALLED ONCE USERNAME IS ENTERED AND EMITTED TO SERVER
+// WHICH WILL HAVE A BLINKING EFFECT ON THE TWO BUTTONS BOLLYWOOD AND HOLLYWOOD.
+    movieOptionAnimation();
+
+// 7] ONCE USERNAME IS ENTERED AND EMITTED THE HOLLWOOD/BOLLYWOOD BUTTONS WILL BE ENABLED FOR USER
+// TO CLICK ON THEM
+    document.querySelectorAll(".movieType").forEach((v)=>{
+        v.style.color='white';
+        v.disabled=false;
+    })  
 }
 
+
+// 6] movieOptionAnimation FUNCTION CODE EXECUTED.
+function movieOptionAnimation() {
+    console.log('INSIDE movieOptionAnimation FUNCTION INSIDE INDEX.JS');
+    let cnt=0;
+    
+        
+            let countdown=setInterval(()=>{
+                if(document.querySelector('#bollywood')!=null) {
+                        if (cnt % 2==0 && cnt < 16) {
+                    
+                            document.querySelector('#bollywood').style.border='2px groove black';
+                            document.querySelector('#hollywood').style.border='2px groove black';
+                
+                        } else if (cnt % 2!=0 && cnt < 16) {
+                            document.querySelector('#bollywood').style.border='';
+                            document.querySelector('#hollywood').style.border='';
+                
+                        } else {
+                            clearInterval(countdown);
+                        }
+                        cnt++;
+                } else {
+                    clearInterval(countdown);
+                }
+             },100);
+        
+}
+
+
+// 7] THE MOVIETYPE CLICKED AND SELECTED BY USER IS EMITTED TO SERVER BY CLIENT. AND START BUTTON TO
+// PLAY THE GAME IS ENABLED FOR USER.
 function movieType(event) {
     console.log('inside movieType:-');
     event.preventDefault();
@@ -48,32 +116,56 @@ function movieType(event) {
     setTimeout(()=>{
         document.getElementById('QuizArea').innerHTML='';
         document.getElementById('QuizArea').innerHTML='<audio id="audio" autoplay=""><source src="" type="audio/mpeg"> </audio><div id="WrongOptions"></div><div id="mydiv"></div><div id="userInpDiv"></div>'
-        document.querySelector('#PrintStatusCont div').innerHTML='CLICK START';
+       
+        clickStartAnimation();
         document.getElementById('startBtn').disabled=false;
         document.getElementById('startBtn').style.color='white';
     },10)
 }
 
-function userOptionsInit(event) {   
-        const username=[];
-        console.log('inside:-userOptionsInit:-');
-        event.preventDefault();
-        document.getElementById('userInpTag').addEventListener('keypress',(event)=>{
-            console.log('event.keyCode:-'+event.keyCode);
-            event.keyCode==13? event.preventDefault() : "";
-        })
-        console.log(event.target.value);
-        username.push(event.target.value);
+// 8] THE FUNCTION CLICK START ANIMATION IS STARTED AFTER MOVIETYPE IS SELECTED BY USER.
+function clickStartAnimation() {
+    console.log('INSIDE clickStartAnimation FUNCTION INSIDE INDEX.JS');
+    let cnt=0;
+    document.querySelector('#PrintStatusCont div').style.fontSize='1.5em';
+    document.querySelector('#PrintStatusCont div').innerHTML='CLICK START';
+    let countdown=setInterval(()=>{
+        if (cnt % 2==0 && cnt < 14) {
+            document.querySelector('#PrintStatusCont div').style.opacity='1';
 
-        username.forEach((v)=>{
-            console.log('emitting:-'+v);
-            socket.emit('userName',v);
-        }); 
-        document.querySelectorAll(".movieType").forEach((v)=>{
-            v.style.color='white';
-            v.disabled=false;
-        })  
+        } else if (cnt % 2!=0 && cnt < 14) {
+            document.querySelector('#PrintStatusCont div').style.opacity='0';
+
+        } else {
+            document.querySelector('#PrintStatusCont div').style.opacity='1';
+
+            document.querySelector('#PrintStatusCont div').style.fontSize='';
+            document.querySelector('#PrintStatusCont div').innerHTML='';
+            clearInterval(countdown);
+        }
+        cnt++;
+    },100);
+
+
 }
+
+// 11] THE MOVIENAME STRING WHICH CONTAINS NAMES OF ALL MOVIES IS RECEIVED BY CLIENT AND STORED IN VARIABLE CALLED
+// OBJ
+socket.on('sendMovieList',(obj)=>{
+// 12] THE OBJ RECEIVED CONTAINS INTIAL SCORE WHICH 0, LIST OF ALL THE MOVIES AND NO OF MOVIES SENT BY USER.
+
+    console.log('sendMovielist received on client..:-'+obj.scr+':'+obj.movieName+':'+obj.movieName.length);
+// 13] AN OBJECT 'hangman' IS MADE OUT OF 'HangmanClass' WITH VARIABLES PASSED AS INTIAL SCORE WHICH 0, LIST OF ALL THE MOVIES 
+// AND NO OF MOVIES SENT BY USER
+    const hangman=new HangmanClass(obj.movieName,undefined,obj.scr,undefined);
+
+// 14] hangman.start() function IS CALLED WHICH SETUPS THE GAME FOR USER.
+    hangman.start();
+    
+})
+
+
+
 
 
 function clicked() {
